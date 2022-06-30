50be034b5bc0ac38bc6f58b763a723aa471faa47
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-06-30 17:42:01 -0300

    Adicionando página _document para carregamento correto das fontes externas

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,10 @@
 import type { AppProps } from "next/app";
-import Head from "next/head";
 import { Global } from "../styles/GlobalStyle";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Global />
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;1,100;1,300;1,400&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
       <Component {...pageProps} />
     </>
   );
